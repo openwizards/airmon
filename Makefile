e20d498e86fb481ebf1cfe5446a7b92b03a3d413
ridicolos <ridicolos@posteo.de>
2025-10-14 21:44:14 +0200

    wip

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 main:
-	mpremote connect /dev/ttyUSB0 cp main.py :main.py
-	mpremote connect /dev/ttyUSB0 reset
-
+	mpremote connect auto cp main.py :main.py
+	mpremote connect auto reset
+reset:
+	esptool.py --chip esp32 erase_flash
+	esptool.py --chip esp32 write_flash -z 0x1000 firmware/ESP32_GENERIC-20250911-v1.26.1.bin
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from machine import Pin
 import time
 
-led = Pin(2, Pin.OUT)  # On-board LED on many ESP32 boards
+# from machine import UART
+from machine import Pin
 
-while True:
-    led.value(not led.value())
-    time.sleep(4)
+pin_0 = Pin(0, Pin.OUT)
+pin_2 = Pin(2, Pin.OUT)
+i = 1
+timeout = 0.1
+
+# while True:
+#    pin_0.on()
+#    pin_2.on()
+#    time.sleep(timeout)
+#    pin_0.off()
+#    pin_2.off()
+#    time.sleep(timeout)
+#    i = i + 1
+#    print(i)
+
+
+baudrate = 115200
+uart = UART(1, baudrate)
+uart.init(baudrate, bits=8, parity=None, stop=1, tx=2, rx=0)
+
+start = [0x00, 0x00, 0x02, 0x01, 0x03, 0xF9]
+start = bytes([0x7E, 0x00, 0x00, 0x02, 0x01, 0x03, 0xF9, 0x7E])
+stop = [0x00, 0x01, 0x00, 0x00, 0xFE]
+read = [0x00, 0x03, 0x00, 0xFC]
+read = bytes([0x7E, 0x00, 0x03, 0x00, 0xFC, 0x7E])
