238c8457751187060e5539048d785330688b867f
Pumacks <daniel_beck@posteo.de>
2025-11-18 19:39:01 +0100

    feat: changed MQ131 to class

--- src/mq131.py
+++ src/mq131.py
@@ -1,18 +1,23 @@
 from machine import ADC, Pin
 import time
 
-adc = ADC(Pin(34, Pin.IN))  # z.B. ESP32
-adc.atten(ADC.ATTN_11DB)  # erlaubt Messung bis ~3.6 V
-adc.width(ADC.WIDTH_12BIT)  # 0–4095
+class MQ131:
+    incoming_voltage = 3.3
+    def __init__(self, pin: Pin):
+        self.adc = ADC(pin)
+        self.adc.atten(ADC.ATTN_11DB)
+        self.adc.width(ADC.WIDTH_12BIT)
 
-def get_ozone_ppb():
-    value = adc.read()
-    voltage = value * 3.3 / 4095
-    #voltage = value * (5/1023)
-    ozone_ppb = 200 * voltage * 0.66     # 1 V = 200 ppb
-    return voltage, ozone_ppb
+    def get_ozone_ppb(self):
+        value = self.adc.read()
+        voltage = value * self.incoming_voltage / 4095
+        ozone_ppb = 200 * voltage * 0.66
+        return voltage, ozone_ppb
 
-while True:
-    v, ppb = get_ozone_ppb()
-    print(f"{v:.2f} V → {ppb:.0f} ppb O₃")
-    time.sleep(1)
+
+if __name__ == "__main__":
+    sensor_mq131 = MQ131(Pin(34, Pin.IN))
+    while True:
+        v, ppb = sensor_mq131.get_ozone_ppb()
+        print(f"{v:.2f} V → {ppb:.0f} ppb O₃")
+        time.sleep(1)
